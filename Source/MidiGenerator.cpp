11d8ade5b92ae0808d2fd53df992e0c831aad288
Liam Lacey <liamlacey@hotmail.com>
2014-12-13 17:28:44 +0000

    Implemented a structure for storing note data, and an algorithm for searching through arrays of this note data to play a sequence. Runs at a very high CPU level though, so need to optimise it (find a way of note having to search through the whole of the arrays each time).

--- Source/MidiGenerator.cpp
+++ Source/MidiGenerator.cpp
@@ -20,6 +20,27 @@ MidiGenerator::MidiGenerator()
         midiOutputDevice->startBackgroundThread();
     else
         std::cout << "Failed to create a virtual MIDI output device!" << std::endl;
+    
+    //default sequence arrays to hold no notes. This will need to be done everytime I new sequence is applied.
+    for (int layer = 0; layer < NUM_OF_LAYERS; layer++)
+    {
+        for (int note = 0; note < SEQ_MAX_NUM_OF_NOTES; note++)
+        {
+            noteSequence[layer][note].note_step_num = NO_NOTE;
+        }
+    }
+    
+    noteSequence[LAYER_MELODY][0].note_step_num = 10;
+    noteSequence[LAYER_MELODY][0].note_chan = 1;
+    noteSequence[LAYER_MELODY][0].note_num = 48;
+    noteSequence[LAYER_MELODY][0].note_vel = 110;
+    noteSequence[LAYER_MELODY][0].note_length = 25;
+    
+    noteSequence[LAYER_MELODY][1].note_step_num = 25;
+    noteSequence[LAYER_MELODY][1].note_chan = 1;
+    noteSequence[LAYER_MELODY][1].note_num = 50;
+    noteSequence[LAYER_MELODY][1].note_vel = 70;
+    noteSequence[LAYER_MELODY][1].note_length = 60;
 }
 
 MidiGenerator::~MidiGenerator()
@@ -30,10 +51,136 @@ MidiGenerator::~MidiGenerator()
 
 void MidiGenerator::run()
 {
+    //This thread runs using Time::getMillisecondCounterHiRes() and a set time interval.
+    //It uses a current step number and searches through all the note sequence arrays
+    //to determine if a note needs to be turned on or off at this particular step in time.
+    
+    //==================================================================================
+    //setup
+    
+    int current_time = Time::getMillisecondCounterHiRes();
+    int step_num = 0;
+    
+    //With this array of NoteMessageData we don't use the length or vel params
+    NoteMessageData note_off_buffer[SIZE_OF_NOTE_OFF_BUF];
+    for (int i = 0; i < SIZE_OF_NOTE_OFF_BUF; i++)
+        note_off_buffer[i].note_step_num = NO_NOTE;
+    
+    //==================================================================================
+    //main loop
+    
     while (!threadShouldExit())
     {
-        wait(1);
-    }
+        //if we have reach the next 'step' in time, process the sequence
+        if (Time::getMillisecondCounterHiRes() >= current_time)
+        {
+            //increase currentTime
+             current_time += SEQ_STEP_INTERVAL;
+            
+            
+            //==================================================================================
+            //search through the note-off buffer for notes that need to be turned off on this step
+            
+            for (int i = 0; i < SIZE_OF_NOTE_OFF_BUF; i++)
+            {
+                //if the notes step number matches the current step number
+                if (note_off_buffer[i].note_step_num == step_num)
+                {
+                    //turn off the note
+                    MidiMessage midiMessage = MidiMessage::noteOff(note_off_buffer[i].note_chan, note_off_buffer[i].note_num);
+                    sendMidiMessage(midiMessage);
+                    
+                    //'remove' this note from the note-off buffer
+                    note_off_buffer[i].note_step_num = NO_NOTE;
+                    
+                } //if (note_off_buffer[i].note_step_num == step_num)
+                
+            } //for (int i = 0; i < SIZE_OF_NOTE_OFF_BUF; i++)
+            
+            //==================================================================================
+            //search through sequence arrays for notes that need to be turned on on this step
+            
+            for (int i = 0; i < SEQ_MAX_NUM_OF_NOTES; i++)
+            {
+                for (int layer = 0; layer < NUM_OF_LAYERS; layer++)
+                {
+                //if this object/note has a matching step number
+                if (noteSequence[layer][i].note_step_num == step_num)
+                {
+                    //play the note
+                    MidiMessage midiMessage = MidiMessage::noteOn(noteSequence[layer][i].note_chan,
+                                                                  noteSequence[layer][i].note_num,
+                                                                  (uint8)noteSequence[layer][i].note_vel);
+                    sendMidiMessage(midiMessage);
+                    
+                    //add this note to the note-off buffer
+                    for (int j = 0; j < SIZE_OF_NOTE_OFF_BUF; j++)
+                    {
+                        //if this index of note_off_buffer is currently not being used
+                        if (note_off_buffer[j].note_step_num == NO_NOTE)
+                        {
+                            //set the note_step_num to be the current step number + the notes length
+                            //<ay need to wrap round to the beggining for step number
+                            int off_step = step_num + noteSequence[layer][i].note_length;
+                            
+                            if (off_step >= SEQ_MAX_NUM_OF_STEPS)
+                                off_step -= SEQ_MAX_NUM_OF_STEPS;
+                            
+                            note_off_buffer[j].note_step_num = off_step;
+                            
+                            note_off_buffer[j].note_chan = noteSequence[layer][i].note_chan;
+                            note_off_buffer[j].note_num = noteSequence[layer][i].note_num;
+                            break;
+                            
+                        } //if (note_off_buffer[j].note_step_num == NO_NOTE)
+                        
+                    } //for (int j = 0; j < SIZE_OF_NOTE_OFF_BUF; j++)
+                    
+                } //if (melodySequence[i].note_step_num == step_num)
+                    
+                } //for (int layer = 0; layer < NUM_OF_LAYERS; layer++)
+                
+            } //for (int i = 0; i < SEQ_MAX_NUM_OF_NOTES; i++)
+            
+            //==================================================================================
+            std::cout << step_num << " ";
+            
+            //increase step_num so we process the next
+            step_num++;
+            
+            if (step_num >= SEQ_MAX_NUM_OF_STEPS)
+                step_num = 0;
+            
+        } //if (Time::getMillisecondCounterHiRes() >= current_time)
+        
+        //should I do this?
+        //wait(1);
+        
+    } //while (!threadShouldExit())
+    
+    //==================================================================================
+    //turn off any 'hanging' notes
+    
+    for (int i = 0; i < SIZE_OF_NOTE_OFF_BUF; i++)
+    {
+        //if this index is a note
+        if (note_off_buffer[i].note_step_num != NO_NOTE)
+        {
+            //turn off the note
+            MidiMessage midiMessage = MidiMessage::noteOff(note_off_buffer[i].note_chan, note_off_buffer[i].note_num);
+            sendMidiMessage(midiMessage);
+            
+            //'remove' this note from the note-off buffer
+            note_off_buffer[i].note_step_num = NO_NOTE;
+            
+        } //if (note_off_buffer[i].note_step_num == step_num)
+        
+    } //for (int i = 0; i < SIZE_OF_NOTE_OFF_BUF; i++)
+}
+
+void MidiGenerator::sendMidiMessage (MidiMessage midiMessage)
+{
+    midiOutputDevice->sendBlockOfMessages(MidiBuffer(midiMessage), Time::getMillisecondCounter(), 44100);
 }
 
 
--- Source/MidiGenerator.h
+++ Source/MidiGenerator.h
@@ -13,6 +13,47 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
+//This is my first attempt at a struct for storing note data so we can play a sequence of notes.
+//When searching through arrays of this struct, it should only process the object/note
+//if the current time/step matches the step number. This method could prove to be CPU extensive
+//as for each step in the global seqeuence it will need to check all array objects for matching
+//values. Struggling to think of a better way though right now though!
+//We could optimse this method by sorting the arrays by note_step_num, and then this will make
+//it possible to only have to search through a particular range of the array, not the whole thing.
+
+struct NoteMessageData
+{
+    int note_step_num; //timestamp
+    int note_chan;
+    int note_num;
+    int note_vel;
+    int note_length; //in step numbers
+};
+
+#define NUM_OF_LAYERS NUM_IMG_SECTIONS
+
+enum SequencerLayers
+{
+    LAYER_MELODY = 0,
+    LAYER_PAD,
+    LAYER_BASS,
+    LAYER_PERC
+};
+
+//FIXME: should this be a variable instead that is used to change the tempo?
+#define SEQ_STEP_INTERVAL 100
+
+#define SEQ_MAX_NUM_OF_STEPS 64
+#define SEQ_MAX_NUM_OF_NOTES 512
+#define SIZE_OF_NOTE_OFF_BUF 64
+
+#define SEQ_MELODY_CHAN 1
+#define SEQ_PAD_CHAN 2
+#define SEQ_BASS_CHAN 3
+#define SEQ_PERC_CHAN 4
+
+#define NO_NOTE -1
+
 class MidiGenerator : public Thread
 {
     
@@ -24,6 +65,8 @@ public:
     
     //Thread callback function
     void run();
+    
+    void sendMidiMessage (MidiMessage midiMessage);
     
     void setAverageRed (int section_num, float value);
     void setAverageGreen (int section_num, float value);
@@ -44,6 +87,7 @@ public:
 private:
     //==============================================================================
     
+    //virtual MIDI output device
     ScopedPointer<MidiOutput> midiOutputDevice;
     
     //variables to store the average colour values for each image section (including a whole-image average)
@@ -54,6 +98,10 @@ private:
     float averagePerceivedBrightness[NUM_IMG_SECTIONS + 1];
     float averageHue[NUM_IMG_SECTIONS + 1];
     float averageSaturation[NUM_IMG_SECTIONS + 1];
+    
+    //arrays to store sequences of note data
+    NoteMessageData noteSequence[NUM_OF_LAYERS][SEQ_MAX_NUM_OF_NOTES];
+    
 };
 
 
