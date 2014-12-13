f87730bc23a8cd35801164f4c0e0c0abed1e4318
Liam Lacey <liamlacey@hotmail.com>
2014-12-13 14:24:11 +0000

    Implemented gathering of colour data for each four sections as well as for the whole image.

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -20,18 +20,139 @@ MainContentComponent::MainContentComponent()
     else
         std::cout << "Failed to create a virtual MIDI output device!" << std::endl;
     
-    image = ImageCache::getFromFile (File ("/Users/Liam/Pictures/300 Nature and City Full HD Wallpapers 1920 X 1080/Wallpapers/(11).jpg"));
+    //image = ImageCache::getFromFile (File ("/Users/Liam/Pictures/300 Nature and City Full HD Wallpapers 1920 X 1080/Wallpapers/(11).jpg"));
+    image = ImageCache::getFromFile (File ("/Users/Liam/Pictures/300 Nature and City Full HD Wallpapers 1920 X 1080/Wallpapers/(24).jpg"));
+    
+    imageWidth = image.getWidth();
+    imageHeight = image.getHeight();
+    
+    std::cout << "Image Width: " << imageWidth << std::endl;
+    std::cout << "Image Height: " << image.getHeight() << std::endl;
+    std::cout << "Is image RGB?: " << image.isRGB() << std::endl;
+    std::cout << "Is image ARGB?: " << image.isARGB() << std::endl;
+    
+    std::cout << std::endl;
+    
+    //work out the average colour value of each of the 4 sections...
+    
+    int sectionStartX[NUM_IMG_SECTIONS];
+    int sectionStartY[NUM_IMG_SECTIONS];
+    int sectionEndX[NUM_IMG_SECTIONS];
+    int sectionEndY[NUM_IMG_SECTIONS];
+    
+    //section 1 (top left)
+    sectionStartX[0] = 0;
+    sectionStartY[0] = 0;
+    sectionEndX[0] = imageWidth / 2;
+    sectionEndY[0] = imageHeight / 2;
+    
+    //section 2 (top right)
+    sectionStartX[1] = imageWidth / 2;
+    sectionStartY[1] = 0;
+    sectionEndX[1] = imageWidth;
+    sectionEndY[1] = imageHeight / 2;
+    
+    //section 3 (bottom left)
+    sectionStartX[2] = 0;
+    sectionStartY[2] = imageHeight / 2;
+    sectionEndX[2] = imageWidth / 2;
+    sectionEndY[2] = imageHeight;
+    
+    //section 4 (bottom right)
+    sectionStartX[3] = imageWidth / 2;
+    sectionStartY[3] = imageHeight / 2;
+    sectionEndX[3] = imageWidth;
+    sectionEndY[3] = imageHeight;
+    
+    //get the colour values of each section
+    for (int section = 0; section < NUM_IMG_SECTIONS; section++)
+    {
+        std::cout << "Image Section: " << section << std::endl;
+        
+        float accRed = 0;
+        float accGreen = 0;
+        float accBlue = 0;
+        float accBrightness = 0;
+        float accPerceivedBrightness = 0;
+        float accHue = 0;
+        float accSaturation = 0;
+        
+        int  pixelCounter = 0;
+        
+        Colour pixelColour;
+
+        //get the colour values of each pixel of this section
+        for (int pixelX = sectionStartX[section]; pixelX < sectionEndX[section]; pixelX++)
+        {
+            for (int pixelY = sectionStartY[section]; pixelY < sectionEndY[section]; pixelY++)
+            {
+                pixelColour = image.getPixelAt(pixelX, pixelY);
+                
+                accRed += pixelColour.getFloatRed();
+                accGreen += pixelColour.getFloatGreen();
+                accBlue += pixelColour.getFloatBlue();
+                accBrightness += pixelColour.getBrightness();
+                accPerceivedBrightness += pixelColour.getPerceivedBrightness();
+                accHue += pixelColour.getHue();
+                accSaturation += pixelColour.getSaturation();
+                
+                pixelCounter++;
+            }
+        }
+        
+        std::cout << "Pixel Count: " << pixelCounter << std::endl;
+        
+        //work out the average of each colour value
+        averageRed[section] = accRed / (float)pixelCounter;
+        averageGreen[section] = accGreen / (float)pixelCounter;
+        averageBlue[section] = accBlue / (float)pixelCounter;
+        averageBrightness[section] = accBrightness / (float)pixelCounter;
+        averagePerceivedBrightness[section] = accPerceivedBrightness / (float)pixelCounter;
+        averageHue[section] = accHue / (float)pixelCounter;
+        averageSaturation[section] = accSaturation / (float)pixelCounter;
+        
+        std::cout << "Red: " << averageRed[section] << " Blue: " << averageBlue[section] << " Green:" << averageGreen[section] << " Brightness: " << averageBrightness[section] << " Perceived Brightness: " << averagePerceivedBrightness[section] << " Hue: " << averageHue[section] << " Saturation: " << averageSaturation[section] << std::endl;
+        std::cout << std::endl;
+    }
+    
+    //get the colour values of the whole image, based on the values of the 4 sections
+    
+    float accRed = 0;
+    float accGreen = 0;
+    float accBlue = 0;
+    float accBrightness = 0;
+    float accPerceivedBrightness = 0;
+    float accHue = 0;
+    float accSaturation = 0;
+    
+    for (int section = 0; section < NUM_IMG_SECTIONS; section++)
+    {
+        accRed += averageRed[section];
+        accGreen += averageGreen[section];
+        accBlue += averageBlue[section];
+        accBrightness += averageBrightness[section];
+        accPerceivedBrightness += averagePerceivedBrightness[section];
+        accHue += averageHue[section];
+        accSaturation += averageSaturation[section];
+    }
+    
+    averageRed[GLOBAL_SECTION] = accRed / (float)NUM_IMG_SECTIONS;
+    averageGreen[GLOBAL_SECTION] = accGreen / (float)NUM_IMG_SECTIONS;
+    averageBlue[GLOBAL_SECTION] = accBlue / (float)NUM_IMG_SECTIONS;
+    averageBrightness[GLOBAL_SECTION] = accBrightness / (float)NUM_IMG_SECTIONS;
+    averagePerceivedBrightness[GLOBAL_SECTION] = accPerceivedBrightness / (float)NUM_IMG_SECTIONS;
+    averageHue[GLOBAL_SECTION] = accHue / (float)NUM_IMG_SECTIONS;
+    averageSaturation[GLOBAL_SECTION] = accSaturation / (float)NUM_IMG_SECTIONS;
+    
+    std::cout << "Whole Image" << std::endl;
+    std::cout << "Red: " << averageRed[GLOBAL_SECTION] << " Blue: " << averageBlue[GLOBAL_SECTION] << " Green:" << averageGreen[GLOBAL_SECTION] << " Brightness: " << averageBrightness[GLOBAL_SECTION] << " Perceived Brightness: " << averagePerceivedBrightness[GLOBAL_SECTION] << " Hue: " << averageHue[GLOBAL_SECTION] << " Saturation: " << averageSaturation[GLOBAL_SECTION] << std::endl;
+    std::cout << std::endl;
+
+    
+    
+
     addAndMakeVisible(imageComponent = new ImageComponent());
     imageComponent->setImage (image);
-    
-    std::cout << image.getWidth() << std::endl;
-    std::cout << image.getHeight() << std::endl;
-    std::cout << image.isARGB() << std::endl;
-    
-     Colour pixelColour = image.getPixelAt(image.getWidth()-1, 0);
-    
-    std::cout << "Red: " << pixelColour.getFloatRed() << " Blue: " << pixelColour.getFloatBlue() << " Green:" << pixelColour.getFloatGreen() << " Brightness: " << pixelColour.getBrightness() << " Perceived Brightness: " << pixelColour.getPerceivedBrightness() << " Hue: " << pixelColour.getHue() << " Saturation: " << pixelColour.getSaturation() << std::endl;
-    
     
     addAndMakeVisible(playButton = new TextButton());
     playButton->setClickingTogglesState(true);
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -11,6 +11,17 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
+enum ImageSections
+{
+    IMG_SECTION_1 = 0,
+    IMG_SECTION_2,
+    IMG_SECTION_3,
+    IMG_SECTION_4,
+    
+    NUM_IMG_SECTIONS
+};
+
+#define GLOBAL_SECTION NUM_IMG_SECTIONS
 
 //==============================================================================
 /*
@@ -40,13 +51,25 @@ private:
     //==============================================================================
     
     //=========================================
-    //GUI stuff...
+    //GUI/Image stuff...
     ScopedPointer <TextButton> playButton;
+    
     Image image;
+    int imageWidth, imageHeight;
+    
+    //varialbles to store the average colour values for each image section (including a whole-image average)
+    float averageRed[NUM_IMG_SECTIONS + 1];
+    float averageGreen[NUM_IMG_SECTIONS + 1];
+    float averageBlue[NUM_IMG_SECTIONS + 1];
+    float averageBrightness[NUM_IMG_SECTIONS + 1];
+    float averagePerceivedBrightness[NUM_IMG_SECTIONS + 1];
+    float averageHue[NUM_IMG_SECTIONS + 1];
+    float averageSaturation[NUM_IMG_SECTIONS + 1];
+    
     ScopedPointer <ImageComponent> imageComponent;
     
     //=========================================
-    //Backend stuff...
+    //Backend/MIDI stuff...
     ScopedPointer<MidiOutput> midiOutputDevice;
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
